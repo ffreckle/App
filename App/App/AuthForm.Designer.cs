3e4d4c25b9b2799a1322b2119377eb58e80aa64c
ffreckle <dvkarpuhin@bk.ru>
2019-04-27 16:03:45 +0300

    Add Auth

--- App/App/AuthForm.Designer.cs
+++ App/App/AuthForm.Designer.cs
@@ -38,49 +38,51 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(284, 375);
+            this.button1.Location = new System.Drawing.Point(99, 286);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(114, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Вход";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(241, 158);
+            this.textBox1.Location = new System.Drawing.Point(69, 85);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.Size = new System.Drawing.Size(201, 20);
             this.textBox1.TabIndex = 1;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(221, 268);
+            this.textBox2.Location = new System.Drawing.Point(69, 178);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.Size = new System.Drawing.Size(201, 20);
             this.textBox2.TabIndex = 2;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(229, 104);
+            this.label1.Location = new System.Drawing.Point(66, 44);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(38, 13);
             this.label1.TabIndex = 3;
-            this.label1.Text = "label1";
+            this.label1.Text = "Логин";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(229, 235);
+            this.label2.Location = new System.Drawing.Point(66, 137);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.Size = new System.Drawing.Size(45, 13);
             this.label2.TabIndex = 4;
-            this.label2.Text = "label2";
+            this.label2.Text = "Пароль";
             // 
             // linkLabel1
             // 
             this.linkLabel1.AutoSize = true;
-            this.linkLabel1.Location = new System.Drawing.Point(317, 317);
+            this.linkLabel1.Location = new System.Drawing.Point(118, 246);
             this.linkLabel1.Name = "linkLabel1";
             this.linkLabel1.Size = new System.Drawing.Size(72, 13);
             this.linkLabel1.TabIndex = 5;
@@ -92,7 +94,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(332, 450);
             this.Controls.Add(this.linkLabel1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -101,6 +103,7 @@
             this.Controls.Add(this.button1);
             this.Name = "AuthForm";
             this.Text = "Auth";
+            this.Load += new System.EventHandler(this.AuthForm_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
--- App/App/AuthForm.cs
+++ App/App/AuthForm.cs
@@ -7,11 +7,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace App
 {
     public partial class AuthForm : Form
     {
+        Form reg;
+
         public AuthForm()
         {
             InitializeComponent();
@@ -22,6 +25,66 @@ namespace App
             this.Hide();
             RegForm reg = new RegForm();
             reg.Show();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AuthForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection connection = new SqlConnection(Properties.Settings.Default.dbConnectionSettings);
+            connection.Open();
+            String login = textBox1.Text;
+            String pass = textBox2.Text;
+
+            SqlCommand command = new SqlCommand("SELECT * FROM users WHERE login = '" + login + "' AND password = '" + pass + "'", connection);
+            SqlDataReader reader = command.ExecuteReader();
+
+            String role = "", name = "";
+            while (reader.Read())
+            {
+                role = reader[2].ToString();
+                name = reader[3].ToString();
+
+            }
+
+            Form form = null;
+            switch (role)
+            {
+                case "User":
+                    form = new UserForm();
+                    this.Hide();
+                    form.Show();
+                    break;
+
+                case "Director":
+                    form = new AdminForm();
+                    this.Hide();
+                    form.Show();
+                    break;
+
+                case "Ware":
+                    form = new WareForm();
+                    this.Hide();
+                    form.Show();
+                    break;
+
+                case "Manager":
+                    form = new ManagerForm();
+                    this.Hide();
+                    form.Show();
+                    break;
+                default:
+                    MessageBox.Show("Роль не устанволена или пользователь не найден!");
+                    break;
+            }
         }
     }
 }
